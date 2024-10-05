b89a8f60b8f65185a75105056cf4829d043095d7
samnsimson <samnishanth01@gmail.com>
2024-10-05 18:22:44 -0400

    put all models in one file

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,7 @@
-from sqlmodel import SQLModel, Field, Column, JSON, Relationship, Enum
+from sqlmodel import SQLModel, Field, Column, JSON, Relationship
 from typing import Optional, List, Dict, Any
 from uuid import UUID, uuid4
 from datetime import datetime, timezone
-from pydantic import UUID4
-from uuid import uuid4
 import enum
 
 
@@ -12,7 +10,7 @@ class WorkflowStatusEnum(str, enum.Enum):
     PUBLISHED = 'PUBLISHED'
 
 
-class Timestamps:
+class Timestamps(SQLModel):
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
@@ -23,40 +21,40 @@ class UserWorkflowPermission(Timestamps, SQLModel, table=True):
 
 
 class UserModel(SQLModel):
-    id: Optional[UUID4] = Field(default_factory=uuid4, primary_key=True)
-    first_name: str = Field(default=None)
-    last_name: str = Field(default=None)
-    email: str = Field(default=None)
-    phone: str = Field(default=None)
-    password: str = Field(default=None)
+    id: Optional[UUID] = Field(default_factory=uuid4, primary_key=True)
+    first_name: str
+    last_name: str
+    email: str
+    phone: str
+    password: str
     email_verified: bool = Field(default=False)
     phone_verified: bool = Field(default=False)
 
 
 class User(Timestamps, UserModel, table=True):
-    created_workflows: List["Workflow"] = Relationship(back_populates='creator')  # type: ignore
-    updated_workflows: List["Workflow"] = Relationship(back_populates='updater')  # type: ignore
-    permitted_workflows: Optional["Workflow"] = Relationship(back_populates='permitted_users', link_model=UserWorkflowPermission)  # type: ignore
+    created_workflows: List["Workflow"] = Relationship(back_populates='creator')
+    # updated_workflows: List["Workflow"] = Relationship(back_populates='updater')
+    permitted_workflows: List["Workflow"] = Relationship(back_populates='permitted_users', link_model=UserWorkflowPermission)
 
 
 class WorkflowModel(SQLModel):
     id: Optional[UUID] = Field(default_factory=uuid4, primary_key=True)
-    name: str = Field(default=None)
-    title: str = Field(default=None)
+    name: str
+    title: str
     description: Optional[str] = Field(default=None)
-    status: WorkflowStatusEnum = Field(sa_column=Column(Enum(WorkflowStatusEnum)), default=WorkflowStatusEnum.DRAFT)
+    status: WorkflowStatusEnum = Field(default=WorkflowStatusEnum.DRAFT)
     created_by: UUID = Field(foreign_key='user.id')
-    updated_by: UUID = Field(foreign_key='user.id')
+    # updated_by: UUID = Field(foreign_key='user.id')
 
     class Config:
         arbitrary_types_allowed = True
 
 
 class Workflow(Timestamps, WorkflowModel, table=True):
-    intents: List["Intent"] = Relationship(back_populates='workflow')  # type:ignore
-    creator: Optional["User"] = Relationship(back_populates="created_workflows")  # type:ignore
-    updater: Optional["User"] = Relationship(back_populates="updated_workflows")  # type:ignore
-    permitted_users: Optional["User"] = Relationship(back_populates="permitted_workflows", link_model=UserWorkflowPermission)  # type:ignore
+    intents: List["Intent"] = Relationship(back_populates='workflow')
+    creator: Optional["User"] = Relationship(back_populates="created_workflows")
+    # updater: Optional["User"] = Relationship(back_populates="updated_workflows")
+    permitted_users: List["User"] = Relationship(back_populates="permitted_workflows", link_model=UserWorkflowPermission)
 
 
 class IntentModel(SQLModel):
@@ -73,4 +71,4 @@ class IntentModel(SQLModel):
 
 
 class Intent(Timestamps, IntentModel, table=True):
-    workflow: Optional["Workflow"] = Relationship(back_populates='intents')  # type: ignore
+    workflow: Optional["Workflow"] = Relationship(back_populates='intents')
